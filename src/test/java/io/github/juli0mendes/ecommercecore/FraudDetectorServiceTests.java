1530a58687a7e4dfe0eff0ead32b011d0910cf8a
juli0mendes <jmendes@live.com>
2023-10-31 21:17:31 -0300

    test: commented test class

--- src/test/java/io/github/juli0mendes/ecommercecore/FraudDetectorServiceTests.java
+++ src/test/java/io/github/juli0mendes/ecommercecore/FraudDetectorServiceTests.java
@@ -1,13 +1,13 @@
-package io.github.juli0mendes.ecommercecore;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FraudDetectorServiceTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package io.github.juli0mendes.ecommercecore;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FraudDetectorServiceTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
