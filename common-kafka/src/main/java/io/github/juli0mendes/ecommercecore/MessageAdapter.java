cb5274760d4aec08c88754f5ace9e3300edb7e7b
juli0mendes <jmendes@live.com>
2024-02-27 22:11:10 -0300

    feat: correlation id and customize serializer

---
+++ common-kafka/src/main/java/io/github/juli0mendes/ecommercecore/MessageAdapter.java
@@ -0,0 +1,21 @@
+package io.github.juli0mendes.ecommercecore;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+
+public class MessageAdapter implements JsonSerializer<Message> {
+
+    @Override
+    public JsonElement serialize(Message message, Type type, JsonSerializationContext context) {
+        var obj = new JsonObject();
+        obj.addProperty("type", message.getPayload().getClass().getName());
+        obj.add("payload", context.serialize(message.getPayload()));
+        obj.add("correlationId", context.serialize(message.getId()));
+
+        return obj;
+    }
+}
